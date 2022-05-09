98c670b68ab1ebc1b6e9dd59473c1aba236636d0
guiyep <guiyep@gmail.com>
2022-05-10 08:59:12 +1000

    fixing prettier

--- src/offline-queue/queue-fifo-poller/index.ts
+++ src/offline-queue/queue-fifo-poller/index.ts
@@ -11,15 +11,15 @@ const defaultConfig: MessagesPollerConfig = {
 
 export const queueFifoPoller =
   <T>(queue: QueueBuilderObject<T>, config?: QueueFifoPollerConfig) =>
-    (executeMessage: ExecuteMessage<T>): MessagesPollerUnregister =>
-      messagesPoller(
-        {
-          getMessage: () => queue.queueStorage.storage.getFirst(config?.concurrency || 1),
-          hasMessages: () => queue.queueStorage.storage.hasAny(),
-          executeMessage,
-          deleteMessage: () => queue.queueStorage.storage.deleteFirst(config?.concurrency || 1),
-          moveToDlqMessage: (data) => queue.queueStorage.storageDlq.set({ key: '222', value: data }),
-        },
-        { ...defaultConfig, ...(config || {}) },
-        messagesFifoProcessor,
-      );
+  (executeMessage: ExecuteMessage<T>): MessagesPollerUnregister =>
+    messagesPoller(
+      {
+        getMessage: () => queue.queueStorage.storage.getFirst(config?.concurrency || 1),
+        hasMessages: () => queue.queueStorage.storage.hasAny(),
+        executeMessage,
+        deleteMessage: () => queue.queueStorage.storage.deleteFirst(config?.concurrency || 1),
+        moveToDlqMessage: (data) => queue.queueStorage.storageDlq.set({ key: '222', value: data }),
+      },
+      { ...defaultConfig, ...(config || {}) },
+      messagesFifoProcessor,
+    );
